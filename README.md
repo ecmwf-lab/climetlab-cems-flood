7861d34f8afec74e54bc7fd461f2dd52ba7c0c64
maif <Iacopo.Ferrario@ecmwf.int>
2021-05-11 11:11:48 +0100

    init

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# climetlab-cems-flood
 
 ## WIP
+=======
+## cems-flood
+
+A dataset plugin for climetlab for the dataset cems-flood/glofas.
+
+
+Features
+--------
+
+In this README is a description of how to get the cems-flood.
+
+## Datasets description
+
+There are two datasets: 
+
+### 1 : `glofas`
+
+
+### 2
+TODO
+
+
+## Using climetlab to access the data (supports grib, netcdf and zarr)
+
+See the demo notebooks here (https://github.com/ecmwf-lab/climetlab_cems_flood/notebooks
+
+https://github.com/ecmwf-lab/climetlab_cems_flood/notebooks/demo_glofas.ipynb
+[nbviewer] (https://nbviewer.jupyter.org/github/climetlab_cems_flood/blob/main/notebooks/demo_glofas.ipynb) 
+[colab] (https://colab.research.google.com/github/climetlab_cems_flood/blob/main/notebooks/demo_glofas.ipynb) 
+
+The climetlab python package allows easy access to the data with a few lines of code such as:
+```
+
+!pip install climetlab climetlab_cems_flood
+import climetlab as cml
+ds = cml.load_dataset(""cems-flood-glofas", date='20201231',)
+ds.to_xarray()
+```
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+
+import io
+import os
+
+import setuptools
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(file_path, encoding="utf-8").read()
+
+
+package_name = "climetlab_cems_flood"
+
+version = None
+init_py = os.path.join(package_name.replace("-", "_"), "__init__.py")
+for line in read(init_py).split("\n"):
+    if line.startswith("__version__"):
+        version = line.split("=")[-1].strip()[1:-1]
+assert version
+
+
+extras_require = {}
+
+setuptools.setup(
+    name=package_name,
+    version=version,
+    description="A dataset plugin for climetlab for the dataset cems-flood/glofas.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="iacopo ferrario",
+    author_email="iacopo.ferrario@ecmwf.int",
+    url="https://github.com/ecmwf-lab/cems-flood",
+    license="Apache License Version 2.0",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["climetlab>=0.5.6"],
+    extras_require=extras_require,
+    zip_safe=True,
+    entry_points={
+        "climetlab.datasets": [
+            "cems-flood-glofas = climetlab_cems_flood.glofas:Glofas",
+            # "cems-flood-other-dataset = climetlab_cems_flood.other_dataset:OtherDatasetClass",
+        ]
+    },
+    keywords="meteorology",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ],
+)
